626dd94e6f36bf9d1dcb361a63c968fa1d9ed188
Gorlov Andrey <a.b.gorlov@gmail.com>
2024-07-16 10:39:47 +0300

    added base files for database

---
+++ BackEnd/core/config.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+
+BASE_DIR = Path(__file__).parent.parent
+
+DB_PATH = f'{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+
+class DbSettings(BaseModel):
+    url: str = f"postgresql+asyncpg:///{DB_PATH}"
+    # echo: bool = False
+    echo: bool = True
+
+
+class Settings(BaseSettings):
+    api_v1_prefix: str = "/api/v1"
+
+    db: DbSettings = DbSettings()
+
+    # db_echo: bool = True
+
+
+settings = Settings()
